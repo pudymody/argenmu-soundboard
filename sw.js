a576008c2fe6e02b05cd9addb9225b845f510159
pudymody <federicoscodelaro@gmail.com>
2016-04-16 00:10:47 -0300

    Cors enabled

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ self.addEventListener("install", function(event) {
 				.then(function(cache){
 					fetch("sounds.json").then(function(r){ return r.json() }).then(function( sounds ){
 						var soundsUrls = sounds.map(function( item ){
-							return new Request("sounds/" + item.file, { mode : "cors" });
+							return new Request("sounds/" + item.file, { mode : "cors", credentials : "include" });
 						});
 
 						cache.addAll( soundsUrls );
